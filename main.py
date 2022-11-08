48e9268026408dfd2531204e4785a0b1dd6acefb
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2022-11-08 12:39:51 -0600

    Added project files

---
+++ main.py
@@ -0,0 +1,8 @@
+from string import ascii_letters
+
+print("Hello, from Python!")
+
+username = input("What is your username? ")
+print(f"Hello {username}!")
+
+print(f"Look at all these letters! {ascii_letters}")
